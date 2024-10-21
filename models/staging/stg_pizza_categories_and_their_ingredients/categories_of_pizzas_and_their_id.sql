29b779358cda0eb3217b76258ce564830a7e35f1
Niraj Shah <nirajs971998@gmail.com>
2024-10-21 09:41:29 +0000

    updating file names

--- models/staging/stg_pizza_categories_and_their_ingredients/categories_of_pizzas_and_their_id.sql
+++ models/staging/stg_pizza_categories_and_their_ingredients/categories_of_pizzas_and_their_id.sql
@@ -2,4 +2,4 @@ select
 upper(PIZZA_ID) as PIZZA_ID, 
 initcap(PIZZA_NAME) as PIZZA_NAME, 
 upper(CATEGORY) as CATEGORY 
-from distinct_category_and_ingredients
+from stg_distinct_category_and_ingredients
