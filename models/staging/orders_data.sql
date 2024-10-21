6328f4421188542a2202cb657153dd86763f421a
Niraj Shah <nirajs971998@gmail.com>
2024-10-21 07:13:16 +0000

    added incremental logic to orders_data_file

--- models/staging/orders_data.sql
+++ models/staging/orders_data.sql
@@ -1,10 +1,31 @@
-select 
+{{ config(
+    materialized='incremental',
+    unique_key='ORDER_ID',
+    on_schema_change='sync_all_columns') 
+}}
+-- Assuming ORDER_ID is unique for each order
+-- Allows schema changes for new columns.
 
-DISTINCT
-CAST(ORDER_ID AS NUMBER) as ORDER_ID,
-to_date(DATE, 'YYYY-MM-DD') AS DATE,
-to_time(TIME) as TIME
 
 
-from
-{{ source('dominos_pizza', 'orders_data') }}
+WITH source_data AS (
+    -- Pulling all data when running for the first time (full refresh)
+    SELECT 
+        CAST(ORDER_ID AS NUMBER) AS ORDER_ID,
+        TO_DATE(DATE, 'YYYY-MM-DD') AS DATE,
+        TO_TIME(TIME) AS TIME
+    FROM {{ source('dominos_pizza', 'orders_data') }}
+
+    {% if is_incremental() %}
+    -- Only new data is loaded if the model is incremental
+
+        WHERE TO_TIMESTAMP(DATE || ' ' || TIME, 'YYYY-MM-DD HH24:MI:SS') > 
+        (
+            SELECT MAX(TO_TIMESTAMP(DATE || ' ' || TIME, 'YYYY-MM-DD HH24:MI:SS')) 
+            FROM {{ this }}
+        )
+
+    {% endif %}
+)
+
+SELECT * FROM source_data
