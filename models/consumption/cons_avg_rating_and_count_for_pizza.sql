acb0481e3fb5047a00bd3f9db885efb7f8198234
Niraj Shah <nirajs971998@gmail.com>
2024-10-22 16:58:20 +0000

    made the final changes in newly  added model

--- models/consumption/cons_avg_rating_and_count_for_pizza.sql
+++ models/consumption/cons_avg_rating_and_count_for_pizza.sql
@@ -12,6 +12,7 @@ GROUP BY PIZZA_NAME)
 
 SELECT 
 t1.PIZZA_NAME as PIZZA_NAME,
+t3.CATEGORY as CATEGORY,
 avg_ratings,
 PIZZA_COUNTS,
 AVG_PRICE_FOR_PIZZA 
@@ -20,3 +21,6 @@ FROM ANS AS t1
     JOIN
 {{ ref('cons_highest_order_pizza') }} as t2
     ON t1.PIZZA_NAME = t2.PIZZA_NAME
+JOIN
+{{ ref('cur_categories_of_pizzas_and_their_id') }} as t3
+    ON t3.PIZZA_NAME = t1.PIZZA_NAME
